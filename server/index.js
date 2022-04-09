b28a683736eef40899c44e930be353a3537cafa8
Aditya1126 <aditya26.bhl@gmail.com>
2022-04-09 13:47:03 +0530

    some changes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import userRoutes from './routes/user.js';
+import questionRoutes from './routes/Questions.js'
+import answerRoutes from './routes/Answers.js'
 
 const app = express();
 app.use(express.json({limit:"30mb", extended:true}));
@@ -13,6 +15,8 @@ res.send("This is the stackoverflow API")
 })
 
 app.use('/user',userRoutes);
+app.use('/questions',questionRoutes);
+app.use('/answer', answerRoutes)
 
 const PORT = process.env.PORT ||5000
 
